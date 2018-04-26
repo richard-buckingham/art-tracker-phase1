0bbde0d84136f560b68fd5c65f71078a79e4ab0f
richie.buckingham <richie.buckingham@gmail.com>
2018-04-26 12:40:01 +1000

    add assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
+import { Store } from '../store';
 
+// feature modules
+
+// containers
 import { AppComponent } from './app.component';
 
+// components
+
+// routes
+export const ROUTES: Routes = [];
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
+  providers: [
+    Store
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {}
+
+/*
+var config = {
+  apiKey: "AIzaSyCXz7GrHLBs-xlsCrr185iG4v4UrNreq2Y",
+  authDomain: "fitness-app-e668a.firebaseapp.com",
+  databaseURL: "https://fitness-app-e668a.firebaseio.com",
+  projectId: "fitness-app-e668a",
+  storageBucket: "fitness-app-e668a.appspot.com",
+  messagingSenderId: "1014564696462"
+};
+*/
